20a3b28fa6e23d0cc3055e27bdf9f4f6886cb88d
jhonperdomo26 <u20221203988@usco.edu.co>
2024-03-19 06:40:27 -0500

    actualización

---
+++ parcial/js/home.js
@@ -0,0 +1,22 @@
+function cambiar_contenido() {
+    document.getElementById("text").innerHTML = "Ferrari es un fabricante de automóviles superdeportivos con sede en Maranello, provincia de Módena, en la región de Emilia-Romaña, Italia. Esta compañía es el resultante de la idea de su fundador Enzo Ferrari de crear una marca de automóviles de turismo con prestaciones lujosas y deportivas, basándose en los éxitos obtenidos a partir de la creación de su Scuderia Ferrari de automovilismo en 1929. El famoso símbolo de Ferrari es un caballo negro rampante sobre un fondo amarillo y con los colores de la bandera de Italia en posición horizontal en la parte superior, normalmente con las letras SF de Scuderia Ferrari/San Fiorano, cuyo fondo tiene forma de escudo heráldico. En el caso de los coches de calle, tiene forma rectangular.";
+}
+
+// function cambiar_color() {
+
+//     document.getElementById("text").style.color = "rgb(255, 255, 255)";
+//     document.getElementById("body").style.backgroundColor = "rgb(0, 0, 0)";
+
+//     const navList = document.querySelector(".nav");
+
+//     navList.classList.toggle('bg-light');
+
+//     navList.classList.toggle('bg-dark');
+
+//     const btnList = document.querySelectorAll(".btn");
+
+//     btnList.forEach((btn) => {
+//         btn.classList.toggle('asereje');
+//         btn.classList.toggle('btn-dark');
+//     });
+// }
