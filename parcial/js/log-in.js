8556f86b9005efac7b457f658513c12e85a5a893
jhonperdomo26 <u20221203988@usco.edu.co>
2024-03-19 07:59:04 -0500

    actualización-4

---
+++ parcial/js/log-in.js
@@ -0,0 +1,6 @@
+function datos() {
+    var usu = document.getElementById("usuario").value;
+    var pass = document.getElementById("clave").value;
+        
+    console.log("Usuario: " + usu + "\nContraseña: " + pass)
+}
